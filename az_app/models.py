8797afdc23373150788f866b91396e6720fccd61
andre azu <andreazuaa@gmail.com>
2021-12-06 12:55:12 +0300

    add initial migration

--- az_app/models.py
+++ az_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Image(models.Model):
-    image=models.ImageField(upload_to='gallery/', default=None)
+    image=models.ImageField(upload_to='pics_gallery/', default=None)
     image_name=models.CharField(max_length=30)
     image_description=models.TextField()
     
