dfc927c0ed3e68a8c1eac9e93d1f2e46bc5043ef
andre azu <andreazuaa@gmail.com>
2021-12-06 14:45:49 +0300

    add image, save delete and update

--- az_app/models.py
+++ az_app/models.py
@@ -5,14 +5,27 @@ from django.db import models
     
 class Location(models.Model):
     location_area=models.CharField(max_length=30)
+    def __str__(self):
+        return self.location_area
 
 class Category(models.Model):
     category_name=models.CharField(max_length=30)
-
+    
+    def __str__(self):
+        return self.category_name
 
 class Image(models.Model):
     image=models.ImageField(upload_to='pics_gallery/', default=None)
     image_name=models.CharField(max_length=30)
     image_description=models.TextField()
-    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,default=None)
     category=models.ManyToManyField(Category)  
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.update()
