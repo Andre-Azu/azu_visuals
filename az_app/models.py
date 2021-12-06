50342590191ef7fe9caaff31608c7bc37ae9ff04
andre azu <andreazuaa@gmail.com>
2021-12-06 13:01:16 +0300

    add the relationship linkage to the models

--- az_app/models.py
+++ az_app/models.py
@@ -2,13 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Image(models.Model):
-    image=models.ImageField(upload_to='pics_gallery/', default=None)
-    image_name=models.CharField(max_length=30)
-    image_description=models.TextField()
     
 class Location(models.Model):
     location_area=models.CharField(max_length=30)
 
 class Category(models.Model):
     category_name=models.CharField(max_length=30)
+
+
+class Image(models.Model):
+    image=models.ImageField(upload_to='pics_gallery/', default=None)
+    image_name=models.CharField(max_length=30)
+    image_description=models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category=models.ManyToManyField(Category)  
