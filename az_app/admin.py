095b02d29a41699ae197a72bd21d2524be1ec2d1
andre azu <andreazuaa@gmail.com>
2021-12-07 07:53:44 +0300

    add linking of the urls

--- az_app/admin.py
+++ az_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('category',)
+
+admin.site.register(Image,ImageAdmin)
 admin.site.register(Category)
 admin.site.register(Location)
