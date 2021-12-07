8e81491e4f34dadb2429100d5bc9de085f5550c7
andre azu <andreazuaa@gmail.com>
2021-12-07 08:18:16 +0300

    add navbar

--- az_app/views.py
+++ az_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Image
-# Create your views here.
+
 def all_images(request):
     images=Image.objects.all()
-    return render(request, 'az_app/home.html', {'images':images})
+    return render(request, 'home.html', )
