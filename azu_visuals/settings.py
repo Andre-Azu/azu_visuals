5b912dcd15ceb12934165fb535a9cb4cc4a994ce
andre azu <andreazuaa@gmail.com>
2021-12-08 10:22:14 +0300

    changes

--- azu_visuals/settings.py
+++ azu_visuals/settings.py
@@ -54,7 +54,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!h(1qwe$-ml2$jxgt+x5!x_lfm%o#@idfr$p5=+=z82oqw8h8='
+SECRET_KEY = 'django-insecure-!h(1qwe$-ml2$jxftdftftewx5!x_lf5rf@idfr$p5=+=z82oqw8h8='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
